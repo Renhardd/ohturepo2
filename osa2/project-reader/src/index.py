ea43a24a17f08be96b4bc65f471a9ba51a6e3059
Renhardd <rene.kangas12@gmail.com>
2025-11-02 20:40:15 +0200

    Tehtävä 7 asti. Seiskassa käytetty eri urlia TOML:in hakuun

---
+++ osa2/project-reader/src/index.py
@@ -0,0 +1,14 @@
+from project_reader import ProjectReader
+
+
+def main():
+    #Alkuperäinen url ei toimi ja menee tyhjälle sivulle
+    #Kävin kaivamassa HY:n avoimen ohtun tehtäväreposta vastaavan linkin
+    #Alkuperäinen: url = "https://raw.githubusercontent.com/ohjelmistotuotanto-hy/tehtavat/main/viikko2/test-project/pyproject.toml"
+    url = "https://raw.githubusercontent.com/ohjelmistotuotanto-hy-avoin/python-kevat-2021/main/koodi/viikko3/web-login-robot/pyproject.toml"
+    reader = ProjectReader(url)
+    print(reader.get_project())
+
+
+if __name__ == "__main__":
+    main()
---
+++ osa2/project-reader/src/project.py
@@ -0,0 +1,25 @@
+class Project:
+    def __init__(self, name, description, authors, dependencies, dev_dependencies):
+        self.name = name
+        self.description = description
+        self.authors = authors
+        self.dependencies = dependencies
+        self.dev_dependencies = dev_dependencies
+
+    def _stringify_dependencies(self, dependencies):
+        return "\n".join(f"- {dep}" for dep in dependencies) if len(dependencies) > 0 else "-"
+
+    def __str__(self):
+        return (
+            f"Name: {self.name}"
+            f"\nDescription: {self.description or '-'}"
+            f"\n"
+            f"\nAuthors: "
+            f"\n{self._stringify_dependencies(self.authors)}"
+            f"\n"
+            f"\nDependencies: "
+            f"\n{self._stringify_dependencies(self.dependencies)}"
+            f"\n"
+            f"\nDevelopment dependencies: "
+            f"\n{self._stringify_dependencies(self.dev_dependencies)}"
+        )
