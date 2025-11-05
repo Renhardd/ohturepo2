bbac10ed8958b1cbf0a98e5a92c4c26ec2720d53
Renhardd <rene.kangas12@gmail.com>
2025-11-05 15:35:12 +0200

    cmt

--- osa2/project-reader/src/index.py
+++ osa2/project-reader/src/index.py
@@ -3,7 +3,7 @@ from project_reader import ProjectReader
 
 def main():
     #Alkuperäinen url ei toimi ja menee tyhjälle sivulle
-    #Kävin kaivamassa HY:n avoimen ohtun tehtäväreposta vastaavan linkin
+    #Kävin kaivamassa itse toimivan
     #Alkuperäinen: url = "https://raw.githubusercontent.com/ohjelmistotuotanto-hy/tehtavat/main/viikko2/test-project/pyproject.toml"
     url = "https://raw.githubusercontent.com/ohjelmistotuotanto-jyu/tehtavat/refs/heads/main/osa2/test-project/pyproject.toml"
     reader = ProjectReader(url)
